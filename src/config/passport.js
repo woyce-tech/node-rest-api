1adf08c26338282e447e1be578546279e0e210d1
Shyamsinh Parmar <parmarshyamsingh8@gmail.com>
2024-02-25 12:51:21 +0530

    #update

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,7 +1,6 @@
 const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
 const config = require('./config');
 const { tokenTypes } = require('./tokens');
-const { User } = require('../models');
 
 const jwtOptions = {
   secretOrKey: config.jwt.secret,
@@ -13,7 +12,7 @@ const jwtVerify = async (payload, done) => {
     if (payload.type !== tokenTypes.ACCESS) {
       throw new Error('Invalid token type');
     }
-    const user = await User.findById(payload.sub);
+    const user = 'await User.findById(payload.sub)';
     if (!user) {
       return done(null, false);
     }
